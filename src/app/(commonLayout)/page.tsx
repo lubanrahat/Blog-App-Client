74fa6efcefa0d54849030ef20a0e74cc8318661d
lubanrahat <lubanrahat.dev@gmail.com>
2026-01-18 19:04:26 +0600

    added google login

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,4 +1,8 @@
-export default function HomePage() {
+import { authClient } from "@/lib/auth-client"
+
+export default async function HomePage() {
+  const session = await authClient.getSession()
+  console.log(session)
   return (
     <main className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold">Home Pages</h1>
